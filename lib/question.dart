616251cdee5b3cbc14ae37b714fa50135963ef6c
MADAN KUMAR <madankumar051@gmail.com>
2020-05-10 16:43:55 +0530

    second commit

---
+++ lib/question.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/services.dart';
+
+class Question
+{
+  String questionText;
+  bool answer;
+
+  Question(String q, bool a)
+  {
+    questionText = q;
+    answer = a;
+  }
+
+
+}
